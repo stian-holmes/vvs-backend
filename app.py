0e22d431312abaf2297049d7a7a0a4806241ccda
stian-holmes <stian@pentex.no>
2026-06-23 22:54:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-# redeploy fix
-# trigger
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "API fungerer ✅"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
